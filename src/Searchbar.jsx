81a52dfd62a9c76dc586aee5e15f879d3b09edcb
Spencer Hum <spencerhum04@gmail.com>
2024-03-02 15:33:22 -0500

    fix box width

--- src/Searchbar.jsx
+++ src/Searchbar.jsx
@@ -47,14 +47,17 @@ function Searchbar() {
                                 </button>
                             </div>
                             <div className="p-4 md:p-5 space-y-4">
-                                <input type="search" id="default-search" className="block w-full p-3 ps-5 text-sm text-gray-900 border border-gray-400 rounded-lg" placeholder="Title:" />
-                                <input
-                                    type="search"
-                                    id="default-search"
-                                    className="block w-full p-12 ps-5 text-sm text-gray-900 border border-gray-400 rounded-lg"
-                                    placeholder="Description:"
-                                    style={{ paddingTop: '12px' }}
-                                />
+                                <div class="flex items-center justify-center w-full">
+                                    <input type="search" id="default-search" className="block w-full p-3 ps-5 text-sm text-gray-900 border border-gray-400 rounded-lg" placeholder="Title:" />
+                                </div>
+                                <div class="flex items-center justify-center w-full"><input
+                                        type="search"
+                                        id="default-search"
+                                        className="block w-full p-12 ps-5 text-sm text-gray-900 border border-gray-400 rounded-lg"
+                                        placeholder="Description:"
+                                        style={{ paddingTop: '12px' }}
+                                    />
+                                </div>
                                 <div class="flex items-center justify-center w-full">
                                     <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
                                         <div class="flex flex-col items-center justify-center pt-5 pb-6">
@@ -66,9 +69,7 @@ function Searchbar() {
                                         </div>
                                         <input id="dropzone-file" type="file" class="hidden" />
                                     </label>
-                                </div> 
-
-
+                                </div>
                             </div>
                             <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b">
                                 <button onClick={closeModal} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create</button>
