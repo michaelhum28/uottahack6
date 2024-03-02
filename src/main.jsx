b0b113587520d29cdde8985a1f11b41fdb4db7b9
Michael Hum <michaelhum28@gmail.com>
2024-03-02 02:23:53 -0500

    stuff

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,13 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 
-  <Auth0Provider
-  domain="dev-omqfqc0xnhsz38gf.us.auth0.com"
-  clientId="RYQbVD0NrwWRugDgzf6HgeXo2A8ilcx2"
-  authorizationParams={{
-    redirect_uri: window.location.origin
-  }}
-  >
-  <App />
-  </Auth0Provider>,
+//   <Auth0Provider
+//   domain="dev-omqfqc0xnhsz38gf.us.auth0.com"
+//   clientId="RYQbVD0NrwWRugDgzf6HgeXo2A8ilcx2"
+//   authorizationParams={{
+//     redirect_uri: window.location.origin
+//   }}
+//   >
+//   <App />
+//   </Auth0Provider>,
 )
