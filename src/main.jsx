60f3ea0c1e1d9e117eb2d8f7df3b4d03917a22ba
jder053@uottawa.ca <jder053@uottawa.ca>
2024-03-02 02:10:00 -0500

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -7,4 +7,14 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+
+  <Auth0Provider
+  domain="dev-omqfqc0xnhsz38gf.us.auth0.com"
+  clientId="RYQbVD0NrwWRugDgzf6HgeXo2A8ilcx2"
+  authorizationParams={{
+    redirect_uri: window.location.origin
+  }}
+  >
+  <App />
+  </Auth0Provider>,
 )
