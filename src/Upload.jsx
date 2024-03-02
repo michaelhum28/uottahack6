047e871a432625adb67ee2242717cfed6cb2ad09
Michael Hum <michaelhum28@gmail.com>
2024-03-02 02:17:02 -0500

    some ui

---
+++ src/Upload.jsx
@@ -0,0 +1,9 @@
+function Upload(){
+    return (
+        <>
+            <button class="text-black font-bold px-3 rounded-md py-11 hover:bg-gray-200">+</button>
+        </>
+    )
+}
+
+export default Upload;
